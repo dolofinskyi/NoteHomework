13bd84262a5f071c2827c4bf31f8b590a631faf4
Denys Olofinskyi <dolofinskyi>
2023-11-25 15:44:02 +0200

    commit

--- src/main/java/ua/test/project/application/Application.java
+++ src/main/java/ua/test/project/application/Application.java
@@ -2,10 +2,13 @@ package ua.test.project.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({
+		"ua.test.project.note",
+})
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
